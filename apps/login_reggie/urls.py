065ce650327e55668505a1ea8b9a9aa3891640c6
DaniMarek <DaniMarek>
2018-02-01 11:57:57 -0600

    second commit: code updated

--- apps/login_reggie/urls.py
+++ apps/login_reggie/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index),
+  url(r'^registration$', views.registration),
+  url(r'^login$', views.login),
+  url(r'^successlog$', views.successlog),
+  url(r'^successreg$', views.successreg),
+  url(r'^logout$', views.logout),
 ]
